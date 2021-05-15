74d9123cda8061ab12f9f0681ba59d76f579d0d2
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-05-15 16:17:14 -0600

    Add composite/1 to create a composite opponent

--- lib/exglicko2/rating.ex
+++ lib/exglicko2/rating.ex
@@ -9,4 +9,21 @@ defmodule Exglicko2.Rating do
     :deviation,
     :volatility
   ]
+
+  @doc """
+  Creates a "composite player" from the given enumerable of ratings.
+
+  The resulting player will have a rating, deviation, and volatility that is the average of all given players.
+  """
+  def composite(players) when is_list(players) do
+    %__MODULE__{
+      rating: Enum.map(players, & &1.rating) |> mean()
+      deviation: Enum.map(players, & &1.deviation) |> mean()
+      volatility: Enum.map(players, & &1.volatility) |> mean()
+    }
+  end
+
+  defp mean(values) when is_list(values) do
+    Enum.sum(values) / Enum.count(values)
+  end
 end
---
+++ test/exglicko2/rating_test.exs
@@ -0,0 +1,4 @@
+defmodule Exglicko2.RatingTest do
+  use ExUnit.Case
+  doctest Exglicko2.Rating
+end
