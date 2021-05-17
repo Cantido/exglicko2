4b7fd4591791cc9688b2a877fb240fa0a72ee8d4
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-05-17 15:09:54 -0600

    add a test for updating teams
    This will help me refactor everything to make this process easier

--- test/exglicko2_test.exs
+++ test/exglicko2_test.exs
@@ -20,4 +20,39 @@ defmodule Exglicko2Test do
     assert_in_delta 151.52, deviation, 0.01
     assert_in_delta 0.05999, volatility, 0.0001
   end
+
+  test "updating two teams from a single match" do
+    team_one = [
+      Exglicko2.Player.new(0.6, 0.2, 0.06),
+      Exglicko2.Player.new(-0.3, 0.6, 0.06),
+      Exglicko2.Player.new(-1.2, 1.7, 0.06)
+    ]
+    team_two = [
+      Exglicko2.Player.new(-0.6, 0.2, 0.06),
+      Exglicko2.Player.new(0.3, 0.6, 0.06),
+      Exglicko2.Player.new(1.2, 1.7, 0.06)
+    ]
+
+    team_one_results = [
+      {team_two, 1}
+    ]
+
+    team_two_results = [
+      {team_one, 0}
+    ]
+
+    updated_team_one = Exglicko2.Player.update_team(team_one, team_one_results)
+    updated_team_two = Exglicko2.Player.update_team(team_two, team_two_results)
+
+    assert updated_team_one == [
+      %Exglicko2.Player{rating: 0.616975738293235, deviation: 0.20788908025514855, volatility: 0.05999748464208987},
+      %Exglicko2.Player{rating: -0.10448679338664246, deviation: 0.5830044120491541, volatility: 0.059997601601461156},
+      %Exglicko2.Player{rating: 0.30879262132575813, deviation: 1.444054347059935, volatility: 0.05999888823648048}
+    ]
+    assert updated_team_two == [
+      %Exglicko2.Player{rating: -0.616975738293235, deviation: 0.20788908025514855, volatility: 0.05999748464208987},
+      %Exglicko2.Player{rating: 0.10448679338664249, deviation: 0.5830044120491541, volatility: 0.059997601601461156},
+      %Exglicko2.Player{rating: -0.30879262132575813, deviation: 1.444054347059935, volatility: 0.05999888823648048}
+    ]
+  end
 end
